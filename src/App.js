a30e40c17e6fae2e43692fc7a60411f954d15618
enrique-lol <enrique.memije.pro@gmail.com>
2023-01-10 00:43:01 -0600

    githubby

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>VIsit later</p>
+        <p>Visit later</p>
       </header>
     </div>
   );
